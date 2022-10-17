6bb2e6abffa1ad1de2f584f541cda08d43450ac1
Sinisa <sinisha.stojanovic@gmail.com>
2022-10-17 23:46:12 +0200

    Implemented scrolling intro of the movie, almost there yet

--- src/components/MovieOpeningCrawl.js
+++ src/components/MovieOpeningCrawl.js
@@ -5,8 +5,11 @@ import MoviesContext from "../store/movies-context";
 const MovieOpeningCrawl = () => {
   const ctx = useContext(MoviesContext);
   return (
-    <div className={classes["movie-description"]}>
+    <div className={classes["movie-opening_crawl"]}>
       <div className={classes.screen_crawl}>
+        <h1 className={classes.title}>
+          {ctx.swMovies.length !== 0 && ctx.selectedMovie?.title}
+        </h1>
         <p> {ctx.swMovies.length !== 0 && ctx.selectedMovie?.openingText}</p>
       </div>
     </div>
